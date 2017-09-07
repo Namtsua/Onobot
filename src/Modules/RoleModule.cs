efe33eba1644953fdcada14a5f11808bfa85a366
Spencer Austman <spencerlgaustman@gmail.com>
2017-09-06 21:12:15 -0700

    Minor fixes

--- src/Modules/RoleModule.cs
+++ src/Modules/RoleModule.cs
@@ -1,21 +1,24 @@
 using System.Threading.Tasks;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace DiscordBot.Modules
 {
     public class RoleModule : ModuleBase<SocketCommandContext>
     {
+    
         [Command("year")]
         public async Task Year(string year = "")
         {
             if (year.Length == 0)
             {
                 await ReplyAsync(
-                    $"Sorry <@{Context.Message.Author.Id}>, you'll need to enter \"!year\" and a number from 1 to 7 corresponding to your standing (or 0 for Alumni)."
+                    $"Sorry {Context.Message.Author.Mention}, you'll need to enter \"!year\" and a number from 1 to 7 corresponding to your standing (or 0 for Alumni)."
                 );
                 return;
             }
-            await YearAsync(year);
+            
+            await YearAsync(Context.Message.Author, year);
         }
         //public Task Year()
          //   => ReplyAsync(
@@ -24,15 +27,15 @@ namespace DiscordBot.Modules
         [Command("help")]
         public Task Help()
             => ReplyAsync(
-                $"Howdy <@{Context.Message.Author.Id}>, currently only the !year, !program, !ams and !youtube commands are supported, bug Namtsua if you want another feature to be added.");
+                $"Howdy {Context.Message.Author.Mention}, currently only the !year, !program, !ams and !youtube commands are supported, bug Namtsua if you want another feature to be added.");
 
         [Command("youtube")]
         public Task Youtube()
             => ReplyAsync(
                 //$"Hello, I am a bot called <@{Context.Message.Author.Id}> written in Discord.Net 1.0\n");
-                $"Hey <@{Context.Message.Author.Id}>, check out my Youtube channel! https://www.youtube.com/channel/UC8KGT0uZ19f6XJPUwxlvzPQ");
+                $"Hey {Context.Message.Author.Mention}, check out my Youtube channel! https://www.youtube.com/channel/UC8KGT0uZ19f6XJPUwxlvzPQ");
     
-        private async Task YearAsync(string year)
+        private async Task YearAsync(SocketUser user, string year)
         {
             int tag = int.Parse(year);
             string tagName;
@@ -48,6 +51,8 @@ namespace DiscordBot.Modules
                 case 7: tagName = "Seventh Year"; break;
                 default: break;
             }
+
+           // await AddRolesAsync()
 
             
         }
