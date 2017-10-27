499d1372bca12768d4e6ba0d677f7d26b86995d6
Spencer Austman <spencerlgaustman@gmail.com>
2017-10-27 02:12:29 -0700

    Added skeleton for admin module

---
+++ src/Modules/AdminModule.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("nuke")]
+        public Task Ams()
+            => ReplyAsync(
+                "http://streamable.com/gbqjv");
+
+        [Command("purge")]
+        public Task Help()
+            => ReplyAsync(
+                "Howdy <@{Context.Message.Author.Id}>, currently only the !year, !program, !ams and !youtube commands are supported, bug Namtsua if you want another feature to be added.");
+
+        [Command("kick")]
+        public Task Youtube()
+            => ReplyAsync(
+                //$"Hello, I am a bot called <@{Context.Message.Author.Id}> written in Discord.Net 1.0\n");
+                $"Hey <@{Context.Message.Author.Id}>, check out my Youtube channel! https://www.youtube.com/channel/UC8KGT0uZ19f6XJPUwxlvzPQ");
+                
+        [Command("ban")]
+        public Task Help()
+            => ReplyAsync(
+                "Howdy <@{Context.Message.Author.Id}>, currently only the !year, !program, !ams and !youtube commands are supported, bug Namtsua if you want another feature to be added.");
+    }
+}
