c266fce0589989ab507b09452e3b9a314d7b7e3e
Spencer Austman <spencerlgaustman@gmail.com>
2017-10-27 04:58:24 -0700

    Updated to newest version;

--- src/Modules/RoleModule.cs
+++ src/Modules/RoleModule.cs
@@ -9,23 +9,23 @@ namespace DiscordBot.Modules
     public class RoleModule : ModuleBase<SocketCommandContext>
     {
     
-        [Command("year")]
-        public async Task Year(string year = "")
-        {
-            if (year.Length == 0)
-            {
-                await ReplyAsync(
-                    $"Sorry {Context.Message.Author.Mention}, you'll need to enter \"!year\" and a number from 1 to 7 corresponding to your standing (or 0 for Alumni)."
-                );
-                return;
+        // [Command("year")]
+        // public async Task Year(string year = "")
+        // {
+        //     if (year.Length == 0)
+        //     {
+        //         await ReplyAsync(
+        //             $"Sorry {Context.Message.Author.Mention}, you'll need to enter \"!year\" and a number from 1 to 7 corresponding to your standing (or 0 for Alumni)."
+        //         );
+        //         return;
                 
-            }
-            var user = Context.User;
-            var guild = Context.Guild;
-            var role = guild.GetRole(282382712642338817);
+        //     }
+        //   //  var user = Context.User;
+        //  //   var guild = Context.Guild;
+        // //    var role = guild.GetRole(282382712642338817);
 
-            await YearAsync((user as Discord.IGuildUser), role, year);
-        }
+        //    // await YearAsync((user as Discord.IGuildUser), role, year);
+        // }
         //public Task Year()
          //   => ReplyAsync(
           //      $"Hi <@{Context.Message.Author.Id}>, let's assign you a year! What year are you in? \n(Type 0 for Alumni or a number from 1 to 7 corresponding to your standing)");
@@ -41,37 +41,37 @@ namespace DiscordBot.Modules
                 //$"Hello, I am a bot called <@{Context.Message.Author.Id}> written in Discord.Net 1.0\n");
                 $"Hey {Context.Message.Author.Mention}, check out my Youtube channel! https://www.youtube.com/channel/UC8KGT0uZ19f6XJPUwxlvzPQ");
     
-        private async Task YearAsync(Discord.IGuildUser user, SocketRole role, string year)
-        {
-            int tag = int.Parse(year);
-            string tagName = "";
-            switch(tag)
-            {
-                case 0: tagName = "Alumni"; break;
-                case 1: tagName = "First Year"; break;
-                case 2: tagName = "Second Year"; break;
-                case 3: tagName = "Third Year"; break;
-                case 4: tagName = "Fourth Year"; break;
-                case 5: tagName = "Fifth Year"; break;
-                case 6: tagName = "Sixth Year"; break;
-                case 7: tagName = "Seventh Year"; break;
-                default: break;
-            }
+        // private async Task YearAsync(Discord.IGuildUser user, SocketRole role, string year)
+        // {
+        //     int tag = int.Parse(year);
+        //     string tagName = "";
+        //     switch(tag)
+        //     {
+        //         case 0: tagName = "Alumni"; break;
+        //         case 1: tagName = "First Year"; break;
+        //         case 2: tagName = "Second Year"; break;
+        //         case 3: tagName = "Third Year"; break;
+        //         case 4: tagName = "Fourth Year"; break;
+        //         case 5: tagName = "Fifth Year"; break;
+        //         case 6: tagName = "Sixth Year"; break;
+        //         case 7: tagName = "Seventh Year"; break;
+        //         default: break;
+        //     }
 
-            await user.AddRoleAsync(role);
+        //    await user.AddRoleAsync(role);
             // ulong asdf = 355223193700794369;
             // SocketRole fff = Context.Guild.GetRole(asdf);
             // await ReplyAsync("asdf");
           //  await user.AddRoleAsync();
           //  await user.AddRoleAsync()
             //await ReplyAsync($"aqqq {asdf.Id}");
-            // if (user.GuildId != "")
+            // if (user.GuildId  != "")
             // {
             //     return;
             // }
             // await user.AddRoleAsync();
             // return;
             
-        }    
+     //   }    
     }
 }
