b45ea442a987abe85e469932cc67543f40b0e884
Spencer Austman <spencerlgaustman@gmail.com>
2017-10-30 16:36:37 -0700

    Added skeleton for the scheduled commands module

---
+++ src/Modules/ScheduleModule.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Discord.Commands;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Net;
+
+namespace DiscordBot.Modules
+{
+    public class ScheduleModule : ModuleBase<SocketCommandContext>
+    {
+    }
+}
